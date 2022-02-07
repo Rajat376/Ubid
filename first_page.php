a115b3c001eb5d5a1689b22c1332e088aa4a2d20
Rajat376 <89566014+Rajat376@users.noreply.github.com>
2022-02-07 18:29:56 +0530

    as

---
+++ first_page.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    <title>
+</title>
+<link rel="stylesheet" href="global.css">
+</head>
+    <body>
+        <a class="jcbtn">Create Game</a>
+        <a class="jcbtn">Join Game</a>
+        <a class="htp">How to Play!</a>
+</body>
+    </html>
