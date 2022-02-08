a2818d9d5b83a060523169386767d0abdbc6deaa
Rajat376 <89566014+Rajat376@users.noreply.github.com>
2022-02-08 20:46:33 +0530

    rt

--- join_room.php
+++ join_room.php
@@ -1,26 +1,45 @@
+<?php session_start(); ?>
 <html>
     <head>
+        <link rel="stylesheet" href="global.css">
     <title>
 </title>
-<script>
-function getVote() {
+<script type="text/javascript">
+  document.addEventListener('DOMContentLoaded',()=>{document.getElementById('btn');
+  while(1)
+  {btn.click()=true;}
+btn.click()=true;})
+document.addEventListener('mousemove',()=>{document.getElementById('btn');
+  while(1)
+  {btn.click()=true;}
+btn.click()=true;})
+document.addEventListener('mouseenter',()=>{document.getElementById('btn');
+  while(1)
+  {btn.click()=true;}
+btn.click()=true;})
+
+function getData() {
+  
   var xmlhttp=new XMLHttpRequest();
   xmlhttp.onreadystatechange=function() {
     if (this.readyState==4 && this.status==200) {
       document.getElementById("users").innerHTML=this.responseText;
     }
   }
-  xmlhttp.open("GET","first_page.php",true);
+  xmlhttp.open("GET","join_room_back.php",true);
   xmlhttp.send();
 }
+
 </script>
-<link rel="stylesheet" href="global.css">
+
 </head>
-    <body>
-        <div class="parent">
-        <div class="users" id="users"></div>
-        <button onclick="getvote()">get</button>
+    <body >
+        <div class="parent1" id="p1">
+        <div class="users" id="users">
+
+        </div>
+        <button class="hide" id="btn" onclick="getData()">get</button>
 </div>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
 </body>
     </html>
--- join_room_back.php
+++ join_room_back.php
@@ -1,4 +1,17 @@
+<?php session_start(); ?>
 <?php
-echo "abcrtghyujkl;lkjhgvcx</br>
-ewrtyuiol;'";
-?>qwsdfghnm
+ $conn=new mysqli("localhost","root" ,"","ubid");
+ if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+   }
+   $query="Select * from ".$_SESSION['room']."";
+   $result=mysqli_query($conn,$query);
+   while($row=mysqli_fetch_assoc($result))
+   {?>
+   <div class="userbox <?php if($row['username']==$_SESSION['username'])
+                                {echo "me";}?>"><?php echo" ".$row['username']." ".$row['amount']; ?></div>
+                                </br>
+
+<?php
+   }
+?>
