af885ac6c70db74a0c81feb134e14f4e610499a6
g0yalsakshi <sakshigoyal035@gmail.com>
2022-02-14 09:48:43 +0530

    AS

--- first_page.php
+++ first_page.php
@@ -2,17 +2,35 @@
     <head>
     <title>
 </title>
-<link rel="stylesheet" href="global.css">
+<link rel="stylesheet" href="global.css"/>
 </head>
+<meta content ="width=device-width,initial-scale=1" name="viewport"/>
+<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+</br>
+</br>
     <body>
-        <div class="parent">
-        <a class="jcbtn" href="create_game.php">Create Game</a>
-</br>
-        <a class="jcbtn" href="join_game.php">Join Game</a>
-</br>
-        <a class="htp" href="how_to_play.php">How to Play!</a>
-</br>
-</div>
+      <!--  <div class="background"> </div>-->
 
+        
+
+        <div class="parent">
+
+<div class="jcbtn1">
+        <a class="jcbtn" href="create_game.php">Create Game</a></div>
+
+
+<div class="htp1">
+        <a class="htp" href="how_to_play.php">How to Play!</a></div>
+
+
+
+        <div class="jcbtn2">
+        <a class="jcbtn" href="join_game.php" >Join Game</a></div>    
+</br>
+
+<img src="img\docomo.jpg" class="imgd imgd1"/>
+</div>
 </body>
+</br></br></br>
+<footer>Ubid</footer>
     </html>
--- how_to_play.php
+++ how_to_play.php
@@ -8,7 +8,7 @@
         <div class="parent">
         <a class="jcbtn" href="create_game.php">Create Game</a>
 </br>
-        <a class="jcbtn" href="join_game.php">Join Game</a>
+        <a class="jcbtn" href="join_game.php" >Join Game</a>
 </br>
         <a class="htp" href="how_to_play.php">How to Play!</a>
 </br>
